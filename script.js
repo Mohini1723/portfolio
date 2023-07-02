415233e709409c264be662d55bc58565ab2a075c
Mohini <88492002+Mohini1723@users.noreply.github.com>
2023-07-03 00:40:52 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,38 @@
+let menuBtn =  document.querySelector('#menu-btn');
+let menuBar =  document.querySelector('.menu-bar');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('bx-x');
+    menuBar.classList.toggle('active');
+
+}
+window.onscroll = () =>{
+    menu.Btn.classList.remove('bx-x');
+    menu.Btn.classList.remove('active');
+}
+
+
+ScrollReveal({
+
+    reset: true,
+    distance: '100px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-bio h1, .about-resume', {origin: 'left'});
+ScrollReveal().reveal('.home-bio p' , {origin: 'right'});
+ScrollReveal().reveal('.home-bio, heading' , {origin: 'top'});
+ScrollReveal().reveal('.profile-pic, .about-items, .skills-items, .projects-items, .form', {origin: 'bottom'});
+
+
+const animText = new Typed('.animated-text', {
+    strings: ['Web Developer' , 'Public Speaker' , 'React Enthusiast'],
+
+        backSpeed: 100,
+        typeSpeed: 100,
+        backDelay: 600,
+        loop: true
+
+        
+    });
